fe58f8bb644da373e973d9137b472d54ceff39e3
weshy <josephwaweru96@gmail.com>
2022-10-30 22:32:17 +0300

    boolean for backend

--- stage1/views.py
+++ stage1/views.py
@@ -7,7 +7,7 @@ def zuri_api(request):
     return JsonResponse(
         {
             "slackUsername": "itsweshy", 
-            "backend": "true",
+            "backend": True,
             "age": 25,
             "bio": "Hooked on continuous self-improvement, open to new exciting opportunities and passionate about the Django backend space"
         }
